e62e7af17c46f6218f89b7ea481e7f5bec9ed017
phymato <phymatosorus@gmail.com>
2021-05-16 12:45:51 -0700

    Better More Options item removal

--- main.lua
+++ main.lua
@@ -1,11 +1,6 @@
-StartDebug()
-
-
 local mod = RegisterMod("More Options Forever", 1)
 local verbose = true -- make this false if you don't want console spam
 function out(...) if verbose then print(...) end end
-
-local canSpawnBossDrop = false -- Global to prevent a 4th boss drop
 
 -- IF YOU FIND IMPORTANT ITEMS THAT SHOULDN'T GET OVERWRITTEN, ADD THEIR ID's TO THIS LIST
 local imporantItemList = {
@@ -43,6 +38,7 @@ function SpawnTreasure(isBossRoom)
         spawnPos = Isaac.GetFreeNearPosition(spawnPos, 1) -- make sure  it doens't overlap anything
 
         local drop = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, spawnPos, Vector(0,0), nil)
+        drop:ToPickup().TheresOptionsPickup = true
         ::skip::
     end
     out("Spawned 3 collectibles")
@@ -67,6 +63,54 @@ function IsImportantItem(entity)
     return false
 end
 
+--[[ Callbacks ]]--
+
+-- Manage globals
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, function(_)
+    canSpawnBossDrop = false
+end)
+
+-- Override treasure room spawn: remove everything
+mod:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, function(_, entityType, variant, subtype, gridIndex, seed)
+    if Game():GetRoom():GetType() == RoomType.ROOM_TREASURE and not IsImportantItem({SubType = subtype}) then
+        return {EntityType.ENTITY_NULL, 0, 0}
+    end
+end)
+
+-- On first visit, spawn three collectibles in treasure room (there should be nothing in it by default)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, function(_)
+    local room = Game():GetRoom()
+    if room:GetType() == RoomType.ROOM_TREASURE and room:IsFirstVisit() then
+        SpawnTreasure(false)
+    end
+end)
+
+-- Spawn TWO custom collectibles after killing a boss (default drop still happens)
+mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, function(_, entity)
+    if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and Game():GetRoom():GetAliveEnemiesCount() <= 1 then
+        SpawnTreasure(true)
+    end
+end)
+
+--[[ depreciated ]] --
+
+-- Replace default boss collectible spawn with nothing (or coin); prevents a 4th drop
+mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, function(_, entityType, variant, subtype, position, velocity, spawner, seed)
+    --[[if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and variant == PickupVariant.PICKUP_COLLECTIBLE then
+        if not IsImportantItem({SubType = subtype}) then -- pretend-entity that's usable by IsImportantItem
+            out("Overriding boss collectible with coin")
+            return {EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_PENNY, 0}
+        end
+    end]]
+end)
+
+-- When player touches a collectible (that isn't an imporant item,) remove other collectibles in room
+mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, function(_, pickupEntity, colliderEntity, low)
+    --[[if pickupEntity.Variant == PickupVariant.PICKUP_COLLECTIBLE and colliderEntity.Type == EntityType.ENTITY_PLAYER and not IsImportantItem(pickupEntity) then
+        ClearCustomCollectibles(pickupEntity)
+    end]]
+end)
+
 function ClearCustomCollectibles(except)
     local room = Game():GetRoom()
     local entities = Isaac:GetRoomEntities()
@@ -88,49 +132,3 @@ function ClearCustomCollectibles(except)
     end
     out("Done")
 end
-
---[[ Callbacks ]]--
-
--- Manage globals
-mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, function(_)
-    canSpawnBossDrop = false
-end)
-
--- Override treasure room spawn: remove everything
-mod:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, function(_, entityType, variant, subtype, gridIndex, seed)
-    if Game():GetRoom():GetType() == RoomType.ROOM_TREASURE then
-        return {EntityType.ENTITY_NULL, 0, 0}
-    end
-end)
-
--- On first visit, spawn three collectibles in treasure room (there should be nothing in it by default)
-mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, function(_)
-    local room = Game():GetRoom()
-    if room:GetType() == RoomType.ROOM_TREASURE and room:IsFirstVisit() then
-        SpawnTreasure(false)
-    end
-end)
-
--- Replace default boss collectible spawn with nothing (or coin); prevents a 4th drop
-mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, function(_, entityType, variant, subtype, position, velocity, spawner, seed)
-    --[[if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and variant == PickupVariant.PICKUP_COLLECTIBLE then
-        if not IsImportantItem({SubType = subtype}) then -- pretend-entity that's usable by IsImportantItem
-            out("Overriding boss collectible with coin")
-            return {EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_PENNY, 0}
-        end
-    end]]
-end)
-
--- Spawn TWO custom collectibles after killing a boss (default drop still happens)
-mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, function(_, entity)
-    if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and Game():GetRoom():GetAliveEnemiesCount() <= 1 then
-        SpawnTreasure(true)
-    end
-end)
-
--- When player touches a collectible (that isn't an imporant item,) remove other collectibles in room
-mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, function(_, pickupEntity, colliderEntity, low)
-    if pickupEntity.Variant == PickupVariant.PICKUP_COLLECTIBLE and colliderEntity.Type == EntityType.ENTITY_PLAYER and not IsImportantItem(pickupEntity) then
-        ClearCustomCollectibles(pickupEntity)
-    end
-end)
