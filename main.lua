149fb49288099b13064e586f95ff4e58161c45d4
phymato <phymatosorus@gmail.com>
2021-05-16 23:48:02 -0700

    rewrite; using OptionsIndex

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 local mod = RegisterMod("More Options Forever", 1)
-local verbose = true -- make this false if you don't want console spam
-function out(...) if verbose then print(...) end end
+local verbose = true
 
--- IF YOU FIND IMPORTANT ITEMS THAT SHOULDN'T GET OVERWRITTEN, ADD THEIR ID's TO THIS LIST
 local imporantItemList = {
     CollectibleType.COLLECTIBLE_NEGATIVE, 
     CollectibleType.COLLECTIBLE_POLAROID,
@@ -16,119 +14,72 @@ local imporantItemList = {
     668 -- dad's note
 }
 
---[[ Helper stuff ]]--
+function out(...) if verbose then print(...) end end
 
-function RemoveCollectibles(list)
-    for _, item in pairs(list) do
-        if item:Exists() then
-            item.Visible = false
-            item:ToPickup():Morph(EntityType.ENTITY_NULL, 0, 0, false)
-        end
-    end
-    out("Disabled custom collectibles in room")
-    mod:RemoveCallback(ModCallbacks.MC_POST_UPDATE, RemoveCollectibles)
-end
-
-function SpawnTreasure(isBossRoom)
-    local room = Game():GetRoom()
-    for x = -80, 80, 80 do
-        if isBossRoom and x == 0 then goto skip end
-        local spawnPos = room:GetClampedPosition(room:GetCenterPos(), 1)
-        spawnPos.X = spawnPos.X + x
-        spawnPos = Isaac.GetFreeNearPosition(spawnPos, 1) -- make sure  it doens't overlap anything
-
-        local drop = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, spawnPos, Vector(0,0), nil)
-        drop:ToPickup().TheresOptionsPickup = true
-        ::skip::
-    end
-    out("Spawned 3 collectibles")
-end
-
--- drop a soul heart if player has More Options or There's Options
-function SpawnPityReward()
-
-end
-
--- todo: special card; cracked key
-
-function IsImportantItem(entity)
+function IsImportantItem(subtype)
     for _, id in pairs(imporantItemList) do
-        print("Checking "..entity.SubType.."...")
-        if id == entity.SubType then
-            out("IsImportantItem: true")
-            return true
-        end
+        if subtype == id then out("IsImportantItem: true"); return true end
     end
-    out("IsImportantItem: false")
-    return false
+    out("IsImportantItem: false"); return false
 end
 
---[[ Callbacks ]]--
+function SpawnChoiceTreasure(pos)
+    -- prevent out-of-bounds
+    pos = Game():GetRoom():GetClampedPosition(pos, 1)
+    -- spawn collectible
+    local drop = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, pos, Vector(0,0), nil)
+    AssignOptionsPickup(drop:ToPickup())
+end
 
--- Manage globals
-mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, function(_)
-    canSpawnBossDrop = false
-end)
+-- Add property to entityPickup, cross compatible with AB+ and REP
+function AssignOptionsPickup(entityPickup)
+    if entityPickup.OptionsPickupIndex then
+        entityPickup.OptionsPickupIndex = 1 -- repentance
+    else
+        entityPickup.TheresOptionsPickup = true -- afterbirth
+    end
+end
 
 -- Override treasure room spawn: remove everything
 mod:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, function(_, entityType, variant, subtype, gridIndex, seed)
-    if Game():GetRoom():GetType() == RoomType.ROOM_TREASURE and not IsImportantItem({SubType = subtype}) then
+    if Game():GetRoom():GetType() == RoomType.ROOM_TREASURE and not IsImportantItem(subtype) then
         return {EntityType.ENTITY_NULL, 0, 0}
     end
 end)
 
--- On first visit, spawn three collectibles in treasure room (there should be nothing in it by default)
+-- Treasure room: Spawn three collectibles (there should be nothing in it by default)
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, function(_)
     local room = Game():GetRoom()
     if room:GetType() == RoomType.ROOM_TREASURE and room:IsFirstVisit() then
-        SpawnTreasure(false)
-    end
-end)
-
--- Spawn TWO custom collectibles after killing a boss (default drop still happens)
-mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, function(_, entity)
-    if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and Game():GetRoom():GetAliveEnemiesCount() <= 1 then
-        SpawnTreasure(true)
-    end
-end)
-
---[[ depreciated ]] --
-
--- Replace default boss collectible spawn with nothing (or coin); prevents a 4th drop
-mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, function(_, entityType, variant, subtype, position, velocity, spawner, seed)
-    --[[if Game():GetRoom():GetType() == RoomType.ROOM_BOSS and variant == PickupVariant.PICKUP_COLLECTIBLE then
-        if not IsImportantItem({SubType = subtype}) then -- pretend-entity that's usable by IsImportantItem
-            out("Overriding boss collectible with coin")
-            return {EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_PENNY, 0}
+        local center = Game():GetRoom():GetCenterPos()
+        for x = -80, 80, 80 do
+            out("Spawning...")
+            SpawnChoiceTreasure(center + Vector(x, 0))
         end
-    end]]
-end)
-
--- When player touches a collectible (that isn't an imporant item,) remove other collectibles in room
-mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, function(_, pickupEntity, colliderEntity, low)
-    --[[if pickupEntity.Variant == PickupVariant.PICKUP_COLLECTIBLE and colliderEntity.Type == EntityType.ENTITY_PLAYER and not IsImportantItem(pickupEntity) then
-        ClearCustomCollectibles(pickupEntity)
-    end]]
-end)
-
-function ClearCustomCollectibles(except)
-    local room = Game():GetRoom()
-    local entities = Isaac:GetRoomEntities()
-
-    for _, entity in pairs(entities) do
-        if not entity then goto skip end
-        if not entity.Variant then goto skip end
-        if not entity:Exists() then goto skip end
-        if IsImportantItem(entity) then goto skip end
-        if entity.SubType == except.SubType then out("Skipping touched entity") goto skip end
-        if entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
-            if entity.ToPickup then
-                --entity.Visible = false
-                entity:ToPickup():Morph(EntityType.ENTITY_NULL, 0, 0, false)
-                out("Cleared a collectible")
-            end
-        end
-        ::skip::
+        out("Spawned three choice collectibles")
     end
-    out("Done")
-end
+end)
+
+-- Handles spawning more options when a boss is killed
+mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, function(_, entityNPC)
+    local clearedBossRoom = (Game():GetRoom():GetAliveEnemiesCount() <= 1) and
+        (Game():GetRoom():GetType() == RoomType.ROOM_BOSS)
+    if clearedBossRoom == true then
+        -- Spawn two collectibles that are "choices"
+        local center = Game():GetRoom():GetCenterPos()
+        for x = -80, 80, 160 do
+            SpawnChoiceTreasure(center + Vector(x, 0))
+        end
+        out("Spawned two choice collectibles, alongside boss drop")
+    end
+end)
+
+-- When a pickup collectible spawns, if it's not an important item, give it OptionsPickupIndex
+mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, function(_, entityPickup)
+    if not IsImportantItem(entityPickup.SubType) and entityPickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+        AssignOptionsPickup(entityPickup)
+        out("Made drop a choice collectible")
+    else
+        out("Drop is NOT a choice collectible")
+    end
+end)
